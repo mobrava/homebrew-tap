643be11ebeab714575dda99daec535f3826217fa
mobrava <82764703+mobrava@users.noreply.github.com>
2026-09-24 16:56:02 +0900

    clipbara 1.3.2

--- Casks/clipbara.rb
+++ Casks/clipbara.rb
@@ -1,6 +1,6 @@
 cask "clipbara" do
-  version "1.3.1"
-  sha256 "6aef40de39acc56f061bf2f0df2ce056c8dd6db752c824510399fda2dae62b91"
+  version "1.3.2"
+  sha256 "7a1edd3b21353c5fa3d87454c83bacef008c6ef0e5f79f95ec51ebd05614d942"
 
   url "https://github.com/mobrava/Clipbara/releases/download/v#{version}/Clipbara-v#{version}.dmg"
   name "Clipbara"
